38fcf768e038dac46ada95d3e11df01b88efb6e1
sean <seankim21@naver.com>
2021-04-11 02:47:37 +0900

    Recap (06:28)

--- src/server.js
+++ src/server.js
@@ -5,10 +5,15 @@ const app = express();
 const PORT = 4000;
 
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("<strong>Welcome Home</strong>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here." });
 };
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`👁‍  Server listening on port http://localhost:${PORT} 📡`);
