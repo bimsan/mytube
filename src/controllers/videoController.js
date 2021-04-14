c84a0764e9fdf85e3c4bb6ecae8e13de5324fe96
sean <seankim21@naver.com>
2021-04-14 21:12:24 +0900

    Iteration (08:01)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,17 @@
-const fakeUser = {
-  userName: "Sean",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Video #3",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
