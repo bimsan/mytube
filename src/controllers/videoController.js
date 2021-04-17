f418a33e97716321f6276c89bc697a196694400d
sean <seankim21@naver.com>
2021-04-18 03:21:43 +0900

    Creating a Video part Two (12:21)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,9 +22,9 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
@@ -34,6 +34,5 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
