0700159bd7ded0531421b2972e415b02b5cd7b30
sean <seankim21@naver.com>
2021-04-25 00:15:39 +0900

    Login part Two (12:07)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -33,21 +34,31 @@ export const postJoin = async (req, res) => {
     });
   }
 };
+
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
+// 유저이름 db와 비교, bcrypt로 db암호와 비교(compare).
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
     res.status(400).render("login", {
-      pageTitle: "Login",
+      pageTitle,
       errorMessage: "An account with this username does not exists.",
     });
   }
-  // check if password correct
-  res.end();
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password.",
+    });
+  }
+  return res.redirect("/");
 };
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log out");
